5a2fdbbbcba4d09a3901fbe4dcdb785ddb92e88a
ndurbin <nick.durbin@gmail.com>
2020-01-15 07:02:42 -0500

    Adjusted the middleware to check for user session

--- api/middleware/validation/restricted.js
+++ api/middleware/validation/restricted.js
@@ -1,24 +1,10 @@
-const bcrypt = require('bcryptjs')
 const Users = require("../../routes/users/user-model")
 
 async function restricted(req, res, next) {
-  const authError = { message: "Invalid Credentials!" }
-  try {
-    const { username, password } = req.headers
-    if (!username || !password) {
-      return res.status(401).json(authError)
-    }
-    const user = await Users.findBy({ username }).first()
-    if (!user) {
-      return res.status(401).json(authError)
-    }
-    const passwordValid = await bcrypt.compare(password, user.password)
-    if (!passwordValid) {
-      return res.status(401).json(authError)
-    } 
-    next()
-  } catch (err) {
-    next(err)
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    await res.status(401).json({ message: 'you shall not pass!!' });
   }
 }
 
